5a6226865ef61f490db84d53b721f30acf47addf
Ilya Solopov <silru@internet.ru>
2023-03-17 20:47:15 +0700

    some variable names changed. debug comments removed

--- Lab1/html_parser.py
+++ Lab1/html_parser.py
@@ -13,14 +13,13 @@ def get_pages_number(url):
         'div', {"class": 'col-sm-6 text-right'})
     pages_number = pages_tag.text.split()[-2]
     return int(pages_number)+1
-    # или брать ссылку на последнюю страницу из спика, а из нее последнюю цифру. сложнее?
 
 
 def check_http_returncode(url):
     '''
     Функция проверяет код возврата http запроса. 
     '''
-    if (requests.get(url).status_code != 200):
+    if requests.get(url).status_code != 200:
         print("Ответ по данному url не был положительным. Попробуйте другой url.")
         return False
     return True
@@ -49,8 +48,8 @@ def get_descriptions(soup, res_dict):
     Функция находит описания товаров и записывает их в словарь.
     """
     descriptions = soup.find_all('div', {"class": 'description'})
-    for descr in descriptions:
-        text = descr.get_text(' ')
+    for description in descriptions:
+        text = description.get_text(' ')
         res_dict['Описание'].append(text.replace('\n', '')[:-2])
 
 
@@ -59,21 +58,24 @@ def login(cabinet_url):
     Функция вводит значения в строки для логина и пароля личного кабинета.
     Сохраняет скриншот результата попытки входа.
     """
-    #<input type="text" name="email" value="" placeholder="E-Mail адрес" id="input-email" class="form-control"> - поле логина
-    #<input type="password" name="password" value="" placeholder="Пароль" id="input-password" class="form-control"> - поле пароля
-    #<input style="margin-top: 15px; display: inline-block;" type="submit" value="Вход" class="btn btn-primary pull-right"> - кнопка ввода
-    options = webdriver.ChromeOptions() 
-    options.add_experimental_option('excludeSwitches', ['enable-logging']) #чтоб хром не писал ворнинги на какие-то usb
+    options = webdriver.ChromeOptions()
+    # чтоб хром не писал ворнинги на usb
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
 
     try:
-        driver = webdriver.Chrome(options=options,executable_path="Computer-networks\\Lab1\\chromedriver\\chromedriver.exe")
+        driver = webdriver.Chrome(
+            options=options, executable_path="Computer-networks\\Lab1\\chromedriver\\chromedriver.exe")
         driver.get(cabinet_url)
 
-        login_field = driver.find_element(By.NAME,"email")
-        password_field = driver.find_element(By.NAME,"password")
+        login_field = driver.find_element(By.NAME, "email")
         login_field.send_keys("some text")
+        password_field = driver.find_element(By.NAME, "password")
         password_field.send_keys("and some")
-        buttons = driver.find_elements(By.CLASS_NAME,"btn.btn-primary.pull-right") #кнопки "регистрация" и "ввод"
+
+        # кнопки "регистрация" и "ввод"
+        # нажимает на "ввод"
+        buttons = driver.find_elements(
+            By.CLASS_NAME, "btn.btn-primary.pull-right")
         buttons[1].click()
 
         driver.save_screenshot("login result.png")
@@ -84,7 +86,6 @@ def login(cabinet_url):
 
 
 if __name__ == "__main__":
-    # получать на инпуте?
     url = "https://elikon.ru/tele-video-audio/televizory-i-aksessuary/televizory"
     if not check_http_returncode(url):
         exit()
@@ -92,10 +93,8 @@ if __name__ == "__main__":
     pages_number = get_pages_number(url)
     res_dict = {'Название': [], 'Цена': [], 'Описание': []}
 
-    for i in range(1, pages_number):
-        tmp_url = url+"?page={}".format(i)
-        # есть смысл проверять каждую ссылку?
-        # if not check_http_returncode(tmp_url): exit()
+    for page_number in range(1, pages_number):
+        tmp_url = url+"?page={}".format(page_number)
         soup = bs(requests.get(tmp_url).text, "html.parser")
         get_names(soup, res_dict)
         get_prices(soup, res_dict)
@@ -104,12 +103,9 @@ if __name__ == "__main__":
     csv_table = pd.DataFrame(res_dict)
     csv_table.to_csv('result.csv')
 
-    # проверка результата 
-    #csv = pd.read_csv("result.csv", index_col=0)
-    #print(csv)
+    # проверка результата
+    # csv = pd.read_csv("result.csv", index_col=0)
+    # print(csv)
 
-    cabinet_url ="https://elikon.ru/my_account"
+    cabinet_url = "https://elikon.ru/my_account"
     login(cabinet_url)
-
-    
-    
